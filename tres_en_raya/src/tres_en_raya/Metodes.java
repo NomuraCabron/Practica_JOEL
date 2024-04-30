ef41b0408fb67189fd250c1d404b62dca92e47e2
Alex Lopez <152896595+NomuraCabron@users.noreply.github.com>
2024-04-30 21:08:12 +0200

    Add files via upload

--- tres_en_raya/src/tres_en_raya/Metodes.java
+++ tres_en_raya/src/tres_en_raya/Metodes.java
@@ -4,109 +4,106 @@ import java.util.Scanner;
 
 public class Metodes {
 
-    private static final char PLAYER_SYMBOL = 'X';
-    private static final char COMPUTER_SYMBOL = 'O';
-    private static final char EMPTY_CELL = ' ';
+    private static final char JUGADOR = 'X';
+    private static final char RIVAL = 'O';
+    private static final char CASILLA = ' ';
 
-    private static char[][] board;
-    private static int moves;
+    private static char[][] tabler;
+    private static int moviment;
 
-    public static void initializeBoard() {
-        board = new char[3][3];
+    public static void initializarTabler() {
+        tabler = new char[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                board[i][j] = EMPTY_CELL;
+                tabler[i][j] = CASILLA;
             }
         }
-        moves = 0;
+        moviment = 0;
     }
 
-    public static void playGame() {
+    public static void ComenzarJoc() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            displayBoard();
-            playerMove(scanner);
-            if (checkWin(PLAYER_SYMBOL) || isBoardFull()) {
+            GenerarTabler();
+            MovimentJugador(scanner);
+            if (ComprovarWin(JUGADOR) || isBoardFull()) {
                 break;
             }
-            computerMove();
-            if (checkWin(COMPUTER_SYMBOL) || isBoardFull()) {
+            MovimentRival();
+            if (ComprovarWin(RIVAL) || isBoardFull()) {
                 break;
             }
         }
-        displayBoard();
-        if (checkWin(PLAYER_SYMBOL)) {
+        GenerarTabler();
+        if (ComprovarWin(JUGADOR)) {
             System.out.println("¡Felicidades, has ganado!");
-        } else if (checkWin(COMPUTER_SYMBOL)) {
+        } else if (ComprovarWin(RIVAL)) {
             System.out.println("Lo siento, la computadora ha ganado.");
         } else {
             System.out.println("¡Es un empate!");
         }
     }
 
-    private static void displayBoard() {
+    private static void GenerarTabler() {
         System.out.println("-------------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
+                System.out.print(tabler[i][j] + " | ");
             }
             System.out.println();
             System.out.println("-------------");
         }
     }
 
-    private static void playerMove(Scanner scanner) {
+    private static void MovimentJugador(Scanner scanner) {
         int row, col;
         do {
             System.out.print("Ingresa la fila (1-3): ");
             row = scanner.nextInt() - 1;
             System.out.print("Ingresa la columna (1-3): ");
             col = scanner.nextInt() - 1;
-        } while (!isValidMove(row, col));
-        board[row][col] = PLAYER_SYMBOL;
-        moves++;
+        } while (!MovimentValid(row, col));
+        tabler[row][col] = JUGADOR;
+        moviment++;
     }
 
-    private static void computerMove() {
+    private static void MovimentRival() {
         int row, col;
         do {
             row = (int) (Math.random() * 3);
             col = (int) (Math.random() * 3);
-        } while (!isValidMove(row, col));
-        board[row][col] = COMPUTER_SYMBOL;
-        moves++;
+        } while (!MovimentValid(row, col));
+        tabler[row][col] = RIVAL;
+        moviment++;
     }
 
-    private static boolean isValidMove(int row, int col) {
-        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == EMPTY_CELL;
+    private static boolean MovimentValid(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && tabler[row][col] == CASILLA;
     }
 
-    private static boolean checkWin(char symbol) {
+    private static boolean ComprovarWin(char symbol) {
         // Verificar filas
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+            if (tabler[i][0] == symbol && tabler[i][1] == symbol && tabler[i][2] == symbol) {
                 return true;
             }
         }
         // Verificar columnas
         for (int i = 0; i < 3; i++) {
-            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+            if (tabler[0][i] == symbol && tabler[1][i] == symbol && tabler[2][i] == symbol) {
                 return true;
             }
         }
         // Verificar diagonales
-        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+        if (tabler[0][0] == symbol && tabler[1][1] == symbol && tabler[2][2] == symbol) {
             return true;
         }
-        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
-            return true;
-        }
-        return false;
+        return tabler[0][2] == symbol && tabler[1][1] == symbol && tabler[2][0] == symbol;
     }
 
     private static boolean isBoardFull() {
-        return moves == 9;
+        return moviment == 9;
     }
 
 }
--- tres_en_raya/src/tres_en_raya/main.java
+++ tres_en_raya/src/tres_en_raya/main.java
@@ -5,8 +5,8 @@ import static tres_en_raya.Metodes.*;
 public class main {
 
     public static void main(String[] args) {
-        initializeBoard();
-        playGame();
+        initializarTabler();
+        ComenzarJoc();
     }
 
 }
